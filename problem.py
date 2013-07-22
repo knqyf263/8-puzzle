b445542b9a7df951caa28d934378d880c187664e
knqyf263 <put.a.feud.pike011235@gmail.com>
2013-07-23 02:44:53 +0900

    solve this problem

--- problem.py
+++ problem.py
@@ -2,15 +2,54 @@
 
 from copy import deepcopy
 from puzzle import Puzzle
-import sys
+import sys,time
 
 def check_answer():
-	if p.Array == answer.Array:
+	if p.Array == default.Array:
 		print "Clear!!"
 		return True		
 	else:
 		return False
 
+def problem():
+	while True:
+	        s = sys.stdin.readline()
+		s = s.rstrip()
+		if s == 'exit':
+			break
+		elif s == 'l':
+			if space_index[1] == 0:
+				print "Invalid"
+			else:
+				p.swap(space_index[0],space_index[1],space_index[0],space_index[1]-1)
+				space_index[1] -= 1
+		elif s == 'r':
+			if space_index[1] == 2:
+				print "Invalid"
+			else:
+				p.swap(space_index[0],space_index[1],space_index[0],space_index[1]+1)
+				space_index[1] += 1
+		elif s == 'u':
+			if space_index[0] == 0:
+				print "Invalid"
+			else:
+				p.swap(space_index[0],space_index[1],space_index[0] - 1, space_index[1])
+				space_index[0] -= 1
+		elif s == 'd':
+			if space_index[0] == 2:
+				print "Invalid"
+			else:
+				p.swap(space_index[0],space_index[1],space_index[0] + 1, space_index[1])
+				space_index[0] += 1
+		else:
+			print "r:right l:left u:up d:down\n"
+	#        print s
+		p.print_puzzle()
+		if check_answer(): 
+			return True
+			break
+		sys.stdout.flush()
+	
 #define question array
 p = Puzzle([
 		["1","2","3"],
@@ -19,54 +58,28 @@ p = Puzzle([
 
 	]) 
 
-answer = Puzzle(deepcopy(p.Array))
+space_index = [2,2]
+default = Puzzle(deepcopy(p.Array))
 
 print "\nSolve the 8-Puzzle!!\nr:right l:left u:up d:down\n"
 print "Goal"
 p.print_puzzle()
 
-p.shuffle(2)
-print "\nProblem"
-p.print_puzzle()
-#p.swap(0,0,0,1)
-#p.print_puzzle()
 sys.stdout.flush()
 
-#for line in sys.stdin:
-#    print "test" 
-while True:
-        s = sys.stdin.readline()
-	s = s.rstrip()
-	if s == 'exit':
-		break
-	elif s == 'l':
-		if space_index[1] == 0:
-			print "Invalid"
-		else:
-			p.swap(space_index[0],space_index[1],space_index[0],space_index[1]-1)
-			space_index[1] -= 1
-	elif s == 'r':
-		if space_index[1] == 2:
-			print "Invalid"
-		else:
-			p.swap(space_index[0],space_index[1],space_index[0],space_index[1]+1)
-			space_index[1] += 1
-	elif s == 'u':
-		if space_index[0] == 0:
-			print "Invalid"
-		else:
-			p.swap(space_index[0],space_index[1],space_index[0] - 1, space_index[1])
-			space_index[0] -= 1
-	elif s == 'd':
-		if space_index[0] == 2:
-			print "Invalid"
-		else:
-			p.swap(space_index[0],space_index[1],space_index[0] + 1, space_index[1])
-			space_index[0] += 1
-#        print s
+starttime = time.time()
+for i in range(0,50):
+	current = int(time.time() - starttime)
+	if current > 60:
+		print "too slow..."
+		sys.exit()
+	p.Array = deepcopy(default.Array)
+	p.shuffle(i * 2)
+	print "\nProblem %d" % (i + 1)
 	p.print_puzzle()
-	if check_answer(): 
-		print "flag:testtest"
-		break
 	sys.stdout.flush()
+	if problem():
+		continue
 
+print "flag:8puzzle is fun!!"		
+sys.exit()
--- solve.py
+++ solve.py
@@ -1,130 +1,67 @@
-#! /usr/bin/python
+#!/usr/bin/python
 
-from pqueue import *
+import socket
 import time
+import re
+import commands
+import sys,time,string
 
-adjacent = (
-    (1, 3),       # 0
-    (0, 2, 4),    # 1
-    (1, 5),       # 2
-    (0, 4, 6),    # 3
-    (1, 3, 5, 7), # 4
-    (2, 4, 8),    # 5
-    (3, 7),       # 6
-    (4, 6, 8),    # 7
-    (5, 7)        # 8
-)
+move = [["","r","","d"],
+	["l","","r","","d"],
+	["","l","","","","d"],
+	["u","","","","r","","d"],
+	["","u","","l","","r","","d"],
+	["","","u","","l","","","","d"],
+	["","","","u","","","","r"],
+	["","","","","u","","l","","r"],
+	["","","","","","u","","l"]]
+ 
+ADDRESS = ""
+PORT = 12001
 
-OPEN = 0
-CLOSE = 1
-FORE = 0
-BACK = 1
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((ADDRESS, PORT))
+ 
+# fetch data
+data = s.recv(2048)
+print data
+data = s.recv(2048)
+print data
+while True:
+	print data
+	data = data.split("\n")
+	print data
+	for i in range(0,len(data)):
+		if data[i].startswith("Problem"):
+			break
+	print i
+	puzzle = []
+	puzzle.append(data[i+2].split("|"))
+	puzzle.append(data[i+3].split("|"))
+	puzzle.append(data[i+4].split("|"))
+	print puzzle
 
-def make_distance_table(board, wide):
-    size = len(board)
-    table = [[0] * size for _ in xrange(size)]
-    for i in xrange(size):
-        p = board[i]
-        if p == 0: continue
-        x1 = i / wide
-        y1 = i % wide
-        for j in xrange(size):
-            x2 = j / wide
-            y2 = j % wide
-            table[p][j] += max(x1 - x2, x2 - x1)
-            table[p][j] += max(y1 - y2, y2 - y1)
-    return table
+	command = "./solve_puzzle.py %s %s %s %s %s %s %s %s %s" % (puzzle[0][1], puzzle[0][2], puzzle[0][3], puzzle[1][1], puzzle[1][2], puzzle[1][3], puzzle[2][1], puzzle[2][2], "0")
+	print command
+	command_list = commands.getoutput(command)
+	command_list = command_list.split("\n")
+	print command_list
+	space_index = []
+	for command in command_list:
+		command = command.strip("[]")
+		command = command.translate(string.maketrans("", ""), " ") 
+		command = command.split(",")
+		if len(command) == 9:
+			space_index.append(command.index("0"))
+	print space_index
+	answer = []
+	for i in range(1,len(space_index)):
+#		print "before:%d after:%d" %(space_index[i - 1], space_index[i])
+		answer.append(move[space_index[i - 1]][space_index[i]])
+	
+	for command in answer:
+		command += "\n"
+		s.send(command) 
+		data = s.recv(2048)
+		print data	 
 
-def get_distance(board, distance):
-    v = 0
-    for x in xrange(9):
-        p = board[x]
-        if p == 0: continue
-        v += distance[p][x]
-    return v
-
-class State:
-    def __init__(self, board, space, prev, move, dir, kind = OPEN):
-        self.board = board
-        self.space = space
-        self.prev = prev
-        self.move = move
-        self.dir = dir
-        self.kind = kind
-        if dir == FORE:
-            dt = start_distance
-        else:
-            dt = goal_distance
-        if prev is None:
-            self.cost = move + get_distance(board, dt)
-        else:
-            p = board[prev.space]
-            self.cost = prev.cost + 1 - dt[p][space] + dt[p][prev.space]
-
-    def __cmp__(x, y):
-        return x.cost - y.cost
-
-def a_star_search(start, goal):
-    global start_distance, goal_distance
-    q = PQueue()
-    table ={}
-    start_distance = make_distance_table(goal, 3)
-    a = State(start, start.index(0), None, 0, FORE)
-    q.push(a)
-    table[tuple(start)] = a
-    goal_distance = make_distance_table(start, 3)
-    a = State(goal, goal.index(0), None, 0, BACK)
-    q.push(a)
-    table[tuple(goal)] = a
-    while not q.isEmpty():
-        a = q.pop()
-        if a.kind == CLOSE: continue
-        for x in adjacent[a.space]:
-            b = a.board[:]
-            b[a.space] = b[x]
-            b[x] = 0
-            key = tuple(b)
-            if key in table:
-                c = table[key]
-                if a.dir != c.dir:
-                    if a.dir == FORE:
-                        print_answer(a)
-                        print_answer_goal(c)
-                    else:
-                        print_answer(c)
-                        print_answer_goal(a)
-                    return
-                if c.move > a.move + 1:
-                    if c.kind == OPEN:
-                        c.kind = CLOSE
-                        c = State(b, x, a, a.move + 1, a.dir)
-                        table[key] = c
-                    else:
-                        c.prev = a
-                        c.cost = c.cost - c.move + a.move + 1
-                        c.move = a.move + 1
-                        c.kind = OPEN
-                    q.push(c)
-            else:
-                c = State(b, x, a, a.move + 1, a.dir)
-                q.push(c)
-                table[key] = c
-        a.kind = CLOSE
-
-def print_answer(x):
-    if x is not None:
-        print_answer(x.prev)
-        print x.board
-
-def print_answer_goal(x):
-    while x is not None:
-        print x.board
-        x = x.prev
-
-a = [1,2,5,4,8,6,7,3,0]
-goal = [1,2,3,4,5,6,7,8,0]
-
-s = time.clock()
-a_star_search(a, goal)
-e = time.clock()
-print "%.3f" % (e - s)
